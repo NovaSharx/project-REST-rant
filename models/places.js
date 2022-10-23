ac9b9b8bb373c63871aef3f60b163e8ee89a7d15
Davin <davin-dj@hotmail.com>
2022-10-23 18:36:02 -0400

    New places can now be added

---
+++ models/places.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: '/images/h-thai-ml-image.jpg'
+    },
+    {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: '/images/coding-cat-cafe-image.jpg'
+    }
+]
