7e6fc17f3eb4cecbb5c31c0726c2a15cd02cc963
Davin <davin-dj@hotmail.com>
2022-11-02 17:13:33 -0400

    Validation error messages can now be displayed

--- controllers/places.js
+++ controllers/places.js
@@ -17,14 +17,25 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   if (!req.body.pic) {
     req.body.pic = undefined
-}
+  }
   db.Place.create(req.body)
     .then(() => {
       res.redirect('/places')
     })
     .catch(err => {
-      console.log('err', err)
-      res.redirect('error404')
+      if (err && err.name == 'ValidationError') {
+        let message = 'Validation Error: '
+        for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+        }
+
+        res.render('places/new', { message })
+      }
+      else {
+        console.log('err', err)
+        res.render('error404')
+      }
     })
 })
 
